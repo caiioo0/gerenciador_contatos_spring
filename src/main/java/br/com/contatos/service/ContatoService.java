9ee081bea5b1eab5af8c48bd7cc1bc80fa9d79e4
caiioo0 <caio.rruda2019@gmail.com>
2024-05-07 17:57:54 -0300

    criando o service da aplicação

---
+++ src/main/java/br/com/contatos/service/ContatoService.java
@@ -0,0 +1,61 @@
+package br.com.contatos.service;
+
+import br.com.contatos.model.Contato;
+import br.com.contatos.repository.ContatoRepository;
+import org.springframework.cglib.core.Local;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ContatoService {
+
+    private ContatoRepository contatoRepository;
+
+    public Contato gravarContato(Contato contato){
+        return contatoRepository.save(contato);
+    }
+
+    public Contato buscarPorId(Long id){
+
+        Optional<Contato> optionalContato = contatoRepository.findById(id);
+
+        if (optionalContato.isPresent()){
+            return optionalContato.get();
+        } else {
+            throw new RuntimeException("Contato não encontrado");
+        }
+
+    }
+
+    public List<Contato> listarTodosContatos(){
+        return contatoRepository.findAll();
+    }
+
+    public void excluir(Long id){
+        Optional<Contato> optionalContato = contatoRepository.findById(id);
+
+        if (optionalContato.isPresent()){
+            contatoRepository.delete(optionalContato.get());
+        }else {
+            throw new RuntimeException("Contato não encontrado");
+        }
+    }
+
+    public List<Contato> listarTodosContatosAniversariantes(LocalDate dataInicial, LocalDate dataFinal){
+        return contatoRepository.findByDataNascimentoBetween(dataInicial, dataFinal);
+    }
+
+    public Contato atualizarContato(Contato contato){
+        Optional<Contato> optionalContato = contatoRepository.findById(contato.getId());
+        if (optionalContato.isPresent()){
+            return contatoRepository.save(contato);
+        } else {
+            throw new RuntimeException("Contato não encontrado");
+        }
+    }
+
+}
+
